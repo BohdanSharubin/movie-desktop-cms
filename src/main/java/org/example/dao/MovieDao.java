31ae5079a15ea9c0dfb4bfaf17e2ae7026c2c86f
Bohdan Sharubin <Bogdansh13@gmail.com>
2026-04-15 21:43:57 +0300

    refactor: remove redundant methods

--- src/main/java/org/example/dao/MovieDao.java
+++ src/main/java/org/example/dao/MovieDao.java
@@ -3,14 +3,8 @@ package org.example.dao;
 import org.example.models.Movie;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MovieDao extends DAO<Movie> {
-    List<Movie> findAll();
-    Optional<Movie> findById(int id);
-    boolean save(Movie movie);
-    boolean update(Movie movie);
-    boolean delete(int id);
     List<Movie> findByTitle(String title);
     List<Movie> findByYear(int year);
 }
