d1419062d2cf73d73309273a4f066748fcfb7c4f
BohdanSharubin <BohdanSharubin@users.noreply.github.com>
2026-04-29 19:25:58 +0000

    deploy: 8100dfcca861b1bfde91a48424b60a2f857eebbf

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.models"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.dao"},{"l":"org.example.dbhelper"},{"l":"org.example.mappers"},{"l":"org.example.models"}];updateSearchResults();
